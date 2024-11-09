f2051d63a3ad49ce825851b773e0c250da1cac10
Samuel <sammyvilla3006@gmail.com>
2024-11-09 14:39:48 -0500

    Update plataforma.java

--- plataforma.java
+++ plataforma.java
@@ -6,12 +6,14 @@ import java.io.IOException;
 public class plataforma{
 
     public static String[] CargarUsuarios(String archivo){
+
+        
         ArrayList<String> usuarios= new ArrayList<>();
         Arraylist<String> estudiantes = new ArrayList<>();
         ArrayList<String> profesores = new ArrayList<>();
         ArrayList<String> administradores = new ArrayList<>();
 
-        //PRUEBA
+        
 
        
             
