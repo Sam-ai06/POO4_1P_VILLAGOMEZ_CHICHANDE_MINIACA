5d8f36afaeb3f6d60f2796363d94240e71b64358
Samuel <sammyvilla3006@gmail.com>
2024-11-09 14:35:42 -0500

    Update plataforma.java

--- plataforma.java
+++ plataforma.java
@@ -13,31 +13,7 @@ public class plataforma{
 
         //PRUEBA
 
-        try (BufferedReader br= new BufferedReader(new FileReader(archivo))){
-            String linea;
-            while ((linea = br.readLine()) != null) {
-                
-                usuarios.add(linea);
-                
-            }
-        } catch (IOException ex){
-            System.out.println("no es posible leer el archivo"+ex.getMessage());
-            }
-            return usuarios.toArray(new String[0]); ///convierte el arraylist en un array
-        }
-
-        public static IniciarSesion(){
-
-        }
-
-
-
-        //PRUEBA
-    public static void main(String[] args) {
-        String archivo = "C:\\Users\\PC\\Desktop\\ESPOL\\P.O.O\\PROYECTO 1P\\usuarios.txt";
-        String[] usuarios= CargarUsuarios(archivo);
-        for (String usuario : usuarios) {
-            System.out.println(usuario); //prueba para comprobar que los lee y los imprime
+       
             
         }
     }
