1e5b0f86325d24a59695df075a9e8878f97e7fa7
Samuel <sammyvilla3006@gmail.com>
2024-11-09 14:53:32 -0500

    Update plataforma.java

--- plataforma.java
+++ plataforma.java
@@ -8,6 +8,8 @@ public class plataforma{
     ArrayList<String> estudiantes = new ArrayList<>();
     ArrayList<String> profesores = new ArrayList<>();
     ArrayList<String> administradores = new ArrayList<>();
+    ArrayList<String> espacios = new ArrayList<>();
+
 
 
 
