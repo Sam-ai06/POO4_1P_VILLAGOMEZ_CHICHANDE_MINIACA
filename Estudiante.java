c0a4d46ef3b48a27bd72fd8bd0cb59d445687e9a
SamuelChichande <sjchicha@espol.edu.ec>
2024-11-22 06:18:36 -0500

    Update Estudiante.java

--- Estudiante.java
+++ Estudiante.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Estudiante extends Usuario {
 
     private String matricula;
@@ -24,14 +26,53 @@ public class Estudiante extends Usuario {
         this.carrera = carrera;
     }
     @Override
-    public void reservar() {
-        //por llenar
+    public void reservar(String fecha, String espacio, Usuario user) {
+        String codigoR = "";
+        //mostrara los espacios disponibles del tipo de espacio elegido
+        for(String LE: listaEspacio){
+            String[] partesE = LE.split(" | ");
+            if(espacio.toUpperCase().equals(partesE[1]) && partesE[4].equals("DISPONIBLE")){
+                System.out.println("Codigo: "+partesE[0]+" - Nombre: "+partesE[2]+" - Capacidad: "+partesE[3]);
+            }
+        }
+        //elegira el espacio a reservar
+        System.out.println("Ingrese el codigo del espacio que desea reservar");
+        String codigo = sc.nextLine();
+        System.out.println("Ingrese el motivo por el que desea reservar el espacio");
+        String motivo = sc.nextLine();
+        System.out.println("Desea crear la reserva? (S/N)");
+        String crearReserva = sc.nextLine();
+        if(crearReserva.equals("S")){
+            //generar codigo de reserva
+            for(String LR: listaReserva){
+                String[] partesR = LR.split(" | ");
+                codigoR = partesR[0];
+            }
+            int codigoUR = Integer.parseInt(codigoR);
+            codigoUR++;
+            codigoR = String.valueOf(codigoUR);
+            //crear la reserva
+            String linea = codigoR+" | "+user.getCodigo()+" | "+user.cedula+" | "+fecha+" | "+codigo+" | "+espacio.toUpperCase()+" | "+"PENDIENTE"+" | "+motivo;
+            plataforma.EscribirArchivo("reservas.txt", linea);
+        }else{
+            mostrarMenu();
+        }
     }
     @Override
     public void Enviarcorreo(String mensaje) {
        //por llenar
     }
 
+    @Override
+    public int mostrarMenu(){
+        System.out.println("Bienvenido");
+        System.out.println("1.  Reservar");
+        System.out.println("2.  Consultar Reserva");
+        System.out.println("Escoja la opccion que desea realizar");
+        int op = sc.nextInt();
+        sc.nextLine();
+        return op;
+    }
     
 
     
