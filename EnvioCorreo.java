d38c417bf86fbb07d27db67d06736275222c8ad2
Salvadorjmski <jminaca@espol.edu.ec>
2024-11-24 02:46:28 -0500

    Create EnvioCorreo.java
    Esta colocado ya la clase enviocorreo la cree aparte para asi poder con el método abstracto en la clase base Usuario y que las clases derivadas lo sobrescriban, llamando a una clase adicional para manejar el envío de correos electrónicos.

---
+++ EnvioCorreo.java
@@ -0,0 +1,40 @@
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import io.github.cdimascio.dotenv.*;
+import java.util.Properties;
+
+public class EnviarCorreo {
+        Dotenv dot = Dotenv.load();
+
+        String host = dot.get("MAIL_HOST");
+        String port = dot.get("MAIL_PORT");
+        String user = dot.get("MAIL_USER");
+        String pass = dot.get("MAIL_PASS");
+        System.out.println(host);
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host",host);
+        prop.put("mail.smtp.port",port);
+        prop.put("mail.smtp.auth",true);
+        prop.put("mail.smtp.mail.starttls.enable",true);
+
+        Session sesion = Session.getInstance(prop, new Authenticator() {
+            protected PasswordAuthentication getpPasswordAuthentication(){
+                return new PasswordAuthentication(user, pass);
+            }
+        });
+
+        try {
+            Message mes = new MimeMessage(sesion);
+            mes.setFrom(new InternetAddress(user,"APP DE RESERVAS"));
+            mes.setRecipients(Message.RecipientType.TO,InternetAddress.parse("jminaca@espol.edu.ec")); //cambiar lo que hay dentro de internetaddress.parse poner el atributo de la clase estudiante o administrador.
+            mes.setSubject(pass);
+            mes.setText(pass);
+            Transport.send(mes);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        
+        }
