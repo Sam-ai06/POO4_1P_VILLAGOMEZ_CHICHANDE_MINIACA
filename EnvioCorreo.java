f30806938ca28fa0bce5b257f19cf66ab3b7e758
Salvadorjmski <jminaca@espol.edu.ec>
2024-11-24 10:15:03 -0500

    Update EnvioCorreo.java
    agregue el metodo static para ser implementado por las demas clases

--- EnvioCorreo.java
+++ EnvioCorreo.java
@@ -6,35 +6,39 @@ import io.github.cdimascio.dotenv.*;
 import java.util.Properties;
 
 public class EnvioCorreo {
+   public static void enviarCorreo(String destinatario, String asunto, String mensaje) {
         Dotenv dot = Dotenv.load();
-
         String host = dot.get("MAIL_HOST");
         String port = dot.get("MAIL_PORT");
         String user = dot.get("MAIL_USER");
         String pass = dot.get("MAIL_PASS");
-        System.out.println(host);
 
+        // Configuración de propiedades para el correo
         Properties prop = new Properties();
-        prop.put("mail.smtp.host",host);
-        prop.put("mail.smtp.port",port);
-        prop.put("mail.smtp.auth",true);
-        prop.put("mail.smtp.mail.starttls.enable",true);
+        prop.put("mail.smtp.host", host);
+        prop.put("mail.smtp.port", port);
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", true);
 
-        Session sesion = Session.getInstance(prop, new Authenticator() {
-            protected PasswordAuthentication getpPasswordAuthentication(){
+        // Crear sesión
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(user, pass);
             }
         });
 
+        // Crear el mensaje de correo
         try {
-            Message mes = new MimeMessage(sesion);
-            mes.setFrom(new InternetAddress(user,"APP DE RESERVAS"));
-            mes.setRecipients(Message.RecipientType.TO,InternetAddress.parse("jminaca@espol.edu.ec")); //cambiar lo que hay dentro de internetaddress.parse poner el atributo de la clase estudiante o administrador.
-            mes.setSubject(pass);  //modificar el pass por el tema que se requiere
-            mes.setText(pass);     //el texto que se colocara por correo
-            Transport.send(mes);     //envio del mensaje
+            Message mes = new MimeMessage(session);
+            mes.setFrom(new InternetAddress(user, "APP DE RESERVAS"));
+            mes.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
+            mes.setSubject(asunto);
+            mes.setText(mensaje);
+            Transport.send(mes);
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        
         }
+    }
+}
