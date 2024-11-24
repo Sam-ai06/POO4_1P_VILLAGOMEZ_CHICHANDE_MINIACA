60dc60ceef9a9b723f80e4eb2675b2e51966883f
SamuelChichande <sjchicha@espol.edu.ec>
2024-11-24 15:43:29 -0500

    Update Administrador.java

--- clases/Administrador.java
+++ clases/Administrador.java
@@ -3,20 +3,14 @@ public class Administrador extends Usuario {
 
     private String cargo;
 
-    public String getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
-
+    //constructor
     public Administrador(String cedula, String nombre, String apellido, String codigo, String contrasena, String correo,
             String rol, String cargo) {
         super(cedula, nombre, apellido, codigo, contrasena, correo, rol);
         this.cargo = cargo;
     }
 
+    //metodos
     @Override
     public void reservar() {
         //NO RESERVA
@@ -26,11 +20,6 @@ public class Administrador extends Usuario {
     public void Enviarcorreo(String mensaje) {
         //por llenar
     }
-
-    public void gestionarReserva(){
-        //por llenar
-    }
-
     
     public void gestionarReserva(String codigoR){
         //mostrando datos de la reserva
@@ -75,7 +64,41 @@ public class Administrador extends Usuario {
 
     }
     
-
+    @override
+    public void consultarReserva(Date fecha){
+        SimpleDateFormat dateF = new SimpleDateFormat("yyyy-MM-dd");
+        String fechaS = dateF.format(fecha);
+        String codigoE = "";
+        //mostrando la cantidad de reservas creadas
+        System.out.println("Número de reservas creadas: "+contReserva);
+        //mostrando las reservas creadas
+        for(String LR: listaReserva){
+            String[] partesR = LR.split(" | ");
+            if (partesR[3].equals(fechaS)) {
+                for(String LU: listaUsuario){
+                    String[] partesU = LR.split(" | ");
+                    if(partesU[7].equals("E")){
+                        for(String LE: listaEstudiantes){
+                            String[] partesE = LE.split(" | ");
+                            if (partesU[1].equals(partesE[1])) {
+                                System.out.println(partesR[0]+" - "+partesR[6]+" - "+fechaS+" - "+partesU[2]+" "+partesU[3]+" - "+partesE[4]+" - "+"ESTUDIANTE");
+                            }
+                        }      
+                    }else if (partesU[7].equals("P")) {
+                        System.out.println(partesR[0]+" - "+partesR[6]+" - "+fechaS+" - "+partesU[2]+" "+partesU[3]+" - "+partesR[7]+" - "+"PROFESOR");
+                    }
+                }
+            }
+        }
+    }
     
+    //getters y setters
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
     
 }
