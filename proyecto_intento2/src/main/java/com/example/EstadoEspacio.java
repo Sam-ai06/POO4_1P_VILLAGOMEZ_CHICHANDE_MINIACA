ed3a8e6939cb1073cb6e18d1058a0609979e08a6
Samuel <sammyvilla3006@gmail.com>
2024-12-01 17:07:22 -0500

    prueba

--- proyecto_intento2/src/main/java/com/example/EstadoEspacio.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public enum EstadoEspacio {
-    OCUPADO,
-    DISPONIBLE;
-}
-
--- proyecto_intento2/src/main/java/com/example/EstadoReserva.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public enum EstadoReserva {
-    APROBADO,
-    RECHAZADO,
-    PENDIENTE;
-}
